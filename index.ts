2706810a806f84529254759c7f987fd8324a90ea
Jayson Thompson <jaysonthompson922@gmail.com>
2023-10-13 11:08:28 -0700

    Sets up new server

--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
 
-const read = (text: string) => {
-	return text;
-};
+const server = Bun.serve({
+	port: 3000,
+	fetch(req) {
+		const url = new URL(req.url);
 
-const msg = read("Hello World");
+		if (url.pathname === "/") {
+			const body = figlet.textSync("video");
+			return new Response(body);
+		}
+		if (url.pathname === "/about") {
+			return new Response("About Me");
+		}
+		if (url.pathname === "/contact") {
+			return new Response("Contact Us");
+		}
+		if (url.pathname === "/feed") {
+			throw new Error("Could not fetch feed");
+		}
+		return new Response("404!");
+	},
+	error(error) {
+		return new Response(`<pre>${error} \n ${error.stack}</pre>`, {
+			headers: {
+				"Content-Type": "text/html",
+			},
+		});
+	},
+});
 
-console.log(msg);
+console.log(`Listening on PORT http://localhost:${server.port}`);
